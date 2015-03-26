e750ee12ce046f64b01e47e8305c4ad56bebfbd7
Ricardo <ricardo.demauro@gmail.com>
2015-03-26 00:58:36 -0300

    .

---
+++ Src/AdventureWorksCatalog/Shared/ViewModel/Messages/ShareContentMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace AdventureWorksCatalog.ViewModel.Messages
+{
+    public class ShareContentMessage
+    {
+        public DataTransferManager TransferManager { get; set; }
+
+        public DataRequestedEventArgs TransferArgs { get; set; }
+
+        public ShareContentMessage(DataTransferManager transferManager, DataRequestedEventArgs transferArgs)
+        {
+            this.TransferArgs = transferArgs;
+            this.TransferManager = transferManager;
+        }
+    }
+}
